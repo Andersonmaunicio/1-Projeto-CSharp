75c47386e3ba9cbe8236feccd1f65ad6cb4c83e9
Andersonmaunicio <andersonmaunicio@gmail.com>
2025-08-12 12:57:56 -0300

    Referencia realizada

---
+++ ExemploFundamentos/Program.cs
@@ -0,0 +1,51 @@
+﻿using ExemploFundamentos.Common.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Anderson";
+pessoa1.Idade = 34;
+pessoa1.Apresentar();
+
+// string opcao;
+// bool exibirMenu = true;
+
+// while (exibirMenu) //Variável sendo falsa não será executada
+// {   
+//     Console.Clear();
+//     Console.WriteLine("Digite a sua opção");
+//     Console.WriteLine("1 - Cadastrar cliente");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     opcao = Console.ReadLine();
+
+//     switch (opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de cliente");
+//             break;
+
+//         case "2":
+//             Console.WriteLine("Buscar de cliente");
+//             break;
+
+//         case "3":
+//             Console.WriteLine("Apagar cliente");
+//             break;
+
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             exibirMenu = false; //Evita que o menu apareça novamente
+//             break;
+
+//         default:
+//             Console.WriteLine("Opção inválida"); //Caso digitado nenhuma das opções
+//             break;
+
+//     }
+
+// }
+
+
+// Console.WriteLine("O programa se encerrou"); //Encerra o programa
